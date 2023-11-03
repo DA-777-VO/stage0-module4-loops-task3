ed7a19b40e04b58794c0e55b03203c26d3fb84ac
David Apakin <apakin.david@gmail.com>
2023-11-03 23:40:18 +0400

    4.2.3

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -3,5 +3,14 @@ package school.mjc.stage0.loops.task3;
 public class DigitsSum {
     public void printDigitsSum(int t){
 
+        int sum = 0;
+        String ttt = Integer.toString(t);
+
+        for (int i = 0; i < ttt.length(); i++) {
+            sum += (t % 10);
+            t /=10;
+        }
+
+
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -3,5 +3,11 @@ package school.mjc.stage0.loops.task3;
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
 
+        int number = 0;
+        for (int i = 0; i < lengthOfLastNumber; i++) {
+            number = number * 10 + 9;
+        }
+        System.out.println(9 + 99 + 999 + 9999 + number);
+
     }
 }
